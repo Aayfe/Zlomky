61ab0c220601c6eb35e4bf697465bc694329e33e
Jakub Didkovský <jakub@didkovsky.cz>
2026-02-17 13:24:13 +0100

    Doplněny aritmatické operace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,37 @@ public class Main {
         IO.println("============= PROJEKT ZLOMKY =============");
         IO.println("==========================================");
 
-        Zlomek zlomek = new Zlomek(1, 2);
+        Zlomek zlomek = new Zlomek(4, 2);
+        Zlomek zlomekB = new Zlomek(3, 4);
+
         IO.println(zlomek);
+        IO.println(zlomekB);
+
+        IO.println("Součet: \n" + zlomek.plus(zlomekB));
+        IO.println("Rozdíl: \n" + zlomek.minus(zlomekB));
+        IO.println("Součin: \n" + zlomek.krat(zlomekB));
+        IO.println("Podíl: \n" + zlomek.deleno(zlomekB));
+
+        IO.println(zlomek.zkratit());
+
+        Zlomek[] pole = new  Zlomek[3];
+        pole[0] = new Zlomek(1,1);
+        pole[1] = new Zlomek(3,5);
+        pole[2] = new Zlomek(1,5);
+
+        IO.println(prumer(pole));
+
+    }
+    public  static  Zlomek prumer(Zlomek[] pole){
+
+        Zlomek soucet = new Zlomek(0,1);
+
+        for (Zlomek z : pole){
+            soucet.plus(z);
+
+        }
+
+        return soucet.deleno(pole.length);
+
     }
 }
--- src/main/java/cz/uhk/zlomky/data/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/data/Zlomek.java
@@ -24,8 +24,81 @@ public class Zlomek {
         this.jmenovatel = jmenovatel;
     }
 
+    //operace
+
+
+    public Zlomek plus(Zlomek zlomekB){
+
+        int citatel, jmenovatel;
+
+        citatel = this.citatel * zlomekB.jmenovatel + zlomekB.citatel * this.jmenovatel;
+        jmenovatel = this.jmenovatel * zlomekB.jmenovatel;
+        return new Zlomek(citatel, jmenovatel);
+    }
+
+    public Zlomek minus(Zlomek zlomekB){
+
+        int citatel, jmenovatel;
+
+        citatel = this.citatel * zlomekB.jmenovatel - zlomekB.citatel * this.jmenovatel;
+        jmenovatel = this.jmenovatel * zlomekB.jmenovatel;
+        return new Zlomek(citatel, jmenovatel);
+    }
+
+    public Zlomek krat(Zlomek zlomekB){
+
+        int citatel, jmenovatel;
+
+        citatel = this.citatel * zlomekB.citatel;
+        jmenovatel = this.jmenovatel * zlomekB.jmenovatel;
+        return new Zlomek(citatel, jmenovatel);
+    }
+
+    public Zlomek deleno(Zlomek zlomekB){
+
+        int citatel, jmenovatel;
+
+        citatel = this.citatel * zlomekB.jmenovatel;
+        jmenovatel = this.jmenovatel * zlomekB.citatel;
+        return new Zlomek(citatel, jmenovatel);
+    }
+
+    public Zlomek deleno(int num){
+
+        int citatel, jmenovatel;
+
+        citatel = this.citatel;
+        jmenovatel = this.jmenovatel * num;
+        return new Zlomek(citatel, jmenovatel);
+    }
+
+    public Zlomek zkratit(){
+        int delitel = spolecnyDelitel(citatel, jmenovatel);
+        return new Zlomek(citatel/delitel, jmenovatel/delitel);
+
+    }
+    private int spolecnyDelitel(int cisloA, int cisloB){
+
+        int delitel, moduloVysledek;
+
+        if (cisloA<cisloB){
+            int tmp = cisloA;
+            cisloA = cisloB;
+            cisloB = tmp;
+        }
+        do {
+            moduloVysledek = cisloA % cisloB;
+            delitel = cisloB;
+
+            cisloA = cisloB;
+            cisloB = moduloVysledek;
+        }while (moduloVysledek != 0);
+
+        return  delitel;
+    }
+
     @Override
     public String toString() {
-        return String.format("%4d\n-------\n%4d", citatel, jmenovatel);
+        return citatel + "/" +jmenovatel;
     }
 }
